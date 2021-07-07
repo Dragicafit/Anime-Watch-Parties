fb9bb0f86be211896a57fe015a704f12eb9e062e
Damien HERBERT <dherbert98@gmail.com>
2021-07-07 19:48:27 +0200

    better regex for url

--- src/background-scripts/clientConst.js
+++ src/background-scripts/clientConst.js
@@ -1,4 +1,4 @@
 exports.parseUrlWakanim =
-  /^https:\/\/www\.wakanim\.tv\/(?<location>\w+)\/v2\/\w+\/episode\/(?<videoId>\d+)/;
+  /^https:\/\/(?:www\.)?wakanim\.tv\/(?<location>\w+)\/v2\/\w+\/episode\/(?<videoId>\d+)/;
 exports.parseUrlCrunchyroll =
-  /^https:\/\/www\.crunchyroll\.com\/(?:(?<location>\w+)\/)?(?<videoId>[\w\/-]+)/;
+  /^https:\/\/(?:www\.)?crunchyroll\.com\/(?:(?<location>[a-zA-Z]{2})\/)?(?<videoId>[\w-]+\/[\w-]+-\d+)\/?(?:\?.*)?$/;
