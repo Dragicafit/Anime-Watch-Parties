4aae41aa8104d8f24decd5834650feb27e4f2fc1
Damien HERBERT <dherbert98@gmail.com>
2022-01-14 12:13:51 +0100

    fix test

--- __tests__/client/clientEvent.test.ts
+++ __tests__/client/clientEvent.test.ts
@@ -52,6 +52,7 @@ beforeEach(() => {
       changeVideoClientTabListener: jest.fn(),
       changeHostClientTabListener: jest.fn(),
       changeOnlineUsersClientTabListener: jest.fn(),
+      changeNameClientTabListener: jest.fn(),
       createMessageClientTabListener: jest.fn(),
 
       createdTabListener: jest.fn(),
